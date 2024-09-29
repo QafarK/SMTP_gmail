341a4b309318cdba8780454024d593295ecfe24e
QafarK <mirqafar2004@gmail.com>
2024-09-29 23:35:24 +0400

    Add project files.

---
+++ SMTP/ComposeMail.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System.ComponentModel;
+using System.Net.Mail;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace SMTP;
+
+public partial class ComposeMail : Window, INotifyPropertyChanged
+{
+    private bool ccVisiblity;
+    private bool bccVisiblity;
+    private string destination;
+    private string ccTxt;
+    private string bccTxt;
+    private string content="";
+
+    public bool CcVisiblity { get => ccVisiblity; set { ccVisiblity = value; OnPropertyChanged(); } }
+    public bool BccVisiblity { get => bccVisiblity; set { bccVisiblity = value; OnPropertyChanged(); } }
+    public string BccTxt { get => bccTxt; set { bccTxt = value; OnPropertyChanged(); } }
+    public string CcTxt { get => ccTxt; set { ccTxt = value; OnPropertyChanged(); } }
+    public string Destination { get => destination; set { destination = value; OnPropertyChanged(); } }
+    public string MailContent { get => content; set { content = value; OnPropertyChanged(); } }
+    public ComposeMail()
+    {
+        InitializeComponent();
+        DataContext = this;
+        CcVisiblity = false;
+        BccVisiblity = false;
+    }
+
+    private void BccBtn_Click(object sender, RoutedEventArgs e)
+    {
+        BccVisiblity = !BccVisiblity;
+    }
+
+    private void CcBtn_Click(object sender, RoutedEventArgs e)
+    {
+        CcVisiblity = !CcVisiblity;
+    }
+
+
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private void SendBtn_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress("cripnocy@gmail.com");
+            mail.To.Add(Destination);
+            if (BccVisiblity)
+                mail.Bcc.Add(BccTxt); 
+            if (CcVisiblity)
+                mail.CC.Add(CcTxt);
+            mail.Subject = string.Empty;
+            mail.Body = MailContent?.ToString() ?? "Null content";
+
+            smtp.Port = 587;
+            smtp.Credentials = new NetworkCredential("cripnocy@gmail.com", "hjfq qqnq erwr ytue");
+            smtp.EnableSsl = true;
+            smtp.Send(mail);
+        }
+
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
+
+
+
+
+
---
+++ SMTP/MainWindow.xaml.cs
@@ -0,0 +1,222 @@
+﻿using MailKit;
+using MailKit.Net.Imap;
+using MailKit.Search;
+using MimeKit;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace SMTP;
+
+public partial class MainWindow : Window, INotifyPropertyChanged
+{
+    private ObservableCollection<MimeMessage> inboxMails = new();
+    private ObservableCollection<MimeMessage> sentMails = new();
+    private ObservableCollection<MimeMessage> snoozedMails = new();
+    private ObservableCollection<MimeMessage> draftMails = new();
+    private ObservableCollection<MimeMessage> starredMails = new();
+
+
+    public ObservableCollection<MimeMessage> InboxMails { get => inboxMails; set { inboxMails = value; OnPropertyChanged(); } }
+    public ObservableCollection<MimeMessage> SentMails { get => sentMails; set { sentMails = value; OnPropertyChanged(); } }
+    public ObservableCollection<MimeMessage> SnoozedMails { get => snoozedMails; set { snoozedMails = value; OnPropertyChanged(); } }
+    public ObservableCollection<MimeMessage> DraftMails { get => draftMails; set { draftMails = value; OnPropertyChanged(); } }
+    public ObservableCollection<MimeMessage> StarredMails { get => starredMails; set { starredMails = value; OnPropertyChanged(); } }
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        var a = new MimeMessage();
+        DataContext = this;
+        InboxCommand = new RelayCommand(InboxCommandExecute);
+        SentCommand = new RelayCommand(SentCommandExecute);
+        StarredCommand = new RelayCommand(StarredCommandExecute);
+        SnoozedCommand = new RelayCommand(SnoozedCommandExecute);
+        DraftCommand = new RelayCommand(DraftCommandExecute);
+    }
+    public async Task<(IList<UniqueId> ids, IMailFolder folder)> FunctionExecuter(ListView list, ImapClient imap,  SpecialFolder? specialFolder, string host = "imap.gmail.com",
+        int port = 993, string username = "cripnocy@gmail.com", string password = "hjfq qqnq erwr ytue")
+    {
+        await imap.ConnectAsync(host, port, true);
+        await imap.AuthenticateAsync(username, password);
+
+        IMailFolder folder;
+        if (specialFolder is null)
+            folder = imap.GetFolder("Inbox");
+        else
+            folder = imap.GetFolder(specialFolder.Value);
+
+        await folder.OpenAsync(FolderAccess.ReadOnly);
+        var ids = await folder.SearchAsync(SearchQuery.All);
+
+        return (ids, folder);
+
+    }
+
+    public ICommand InboxCommand { get; set; }
+    public async void InboxCommandExecute(object? obj)
+    {
+        if (obj is ListView list)
+        {
+            try
+            {
+                using var imap = new ImapClient();
+ 
+                var (ids, folder) = await FunctionExecuter(list, imap, null);
+                InboxMails = new ObservableCollection<MimeMessage>(); 
+                list.ItemsSource = InboxMails;
+
+                foreach (var id in ids)
+                {
+                    var message = await folder.GetMessageAsync(id);
+
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        InboxMails.Add(message);
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+
+    public ICommand SentCommand { get; set; }
+    public async void SentCommandExecute(object? obj)
+    {
+        if (obj is ListView list)
+        {
+            try
+            {
+                using var imap = new ImapClient();
+
+                var (ids, folder) = await FunctionExecuter(list, imap, null);
+
+                SentMails = [];
+                list.ItemsSource = SentMails;
+                foreach (var id in ids)
+                {
+                    var message = await folder.GetMessageAsync(id);
+                    Application.Current.Dispatcher.Invoke(new(() =>
+                    {
+                        SentMails.Add(message);
+                    }));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+
+    public ICommand StarredCommand { get; set; }
+    public async void StarredCommandExecute(object? obj)
+    {
+        if (obj is ListView list)
+        {
+            try
+            {
+                using var imap = new ImapClient();
+
+                var (ids, folder) = await FunctionExecuter(list, imap, null);
+
+                StarredMails = [];
+                list.ItemsSource = StarredMails;
+                foreach (var id in ids)
+                {
+                    var message = await folder.GetMessageAsync(id);
+                    Application.Current.Dispatcher.Invoke(new(() =>
+                    {
+                        StarredMails.Add(message);
+                    }));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+
+    public ICommand SnoozedCommand { get; set; }
+    public async void SnoozedCommandExecute(object? obj)
+    {
+        if (obj is ListView list)
+        {
+            try
+            {
+                using var imap = new ImapClient();
+
+                var (ids, folder) = await FunctionExecuter(list, imap, null);
+
+                SnoozedMails = [];
+                list.ItemsSource = SnoozedMails;
+                foreach (var id in ids)
+                {
+                    var message = await folder.GetMessageAsync(id);
+                    Application.Current.Dispatcher.Invoke(new(() =>
+                        SnoozedMails.Add(message)
+                    ));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+
+    public ICommand DraftCommand { get; set; }
+    public async void DraftCommandExecute(object? obj)
+    {
+        if (obj is ListView list)
+        {
+            try
+            {
+                using var imap = new ImapClient();
+
+                var (ids, folder) = await FunctionExecuter(list, imap, null);
+
+                DraftMails = [];
+                list.ItemsSource = DraftMails;
+                foreach (var id in ids)
+                {
+                    var message = await folder.GetMessageAsync(id);
+                    Application.Current.Dispatcher.Invoke(new(() =>
+                    {
+                        DraftMails.Add(message);
+                    }));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+        var window = new ComposeMail();
+        window.Show();
+    }
+
+    private void ListView_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        var window = new ComposeMail();
+        window.Show();
+    }
+}
